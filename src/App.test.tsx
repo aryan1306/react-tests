2c6a7977070fba87e6a14be46f797c4746a5b9f0
Aryan Singhal <aryanchannel@gmail.com>
2023-08-14 18:11:55 +0530

    initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,19 @@
+import { describe, test, expect, afterEach } from "vitest";
+import { cleanup, render } from "@testing-library/react";
+import App from "./App";
+
+describe("Snapshot Testing", () => {
+  afterEach(cleanup);
+  test("snapshot is accurate", () => {
+    const wrapper = render(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe("Counter", () => {
+  test("initial value is 0", () => {
+    const wrapper = render(<App />);
+    const count = wrapper.getByTestId("counter").textContent;
+    expect(count).toEqual("0");
+  });
+});
